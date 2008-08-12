b5c004d50873706671efbc3a2ba6f72291dfe88e
ppkciz <ppkciz@9af58faf-7e3e-0410-b956-55d145112073>
2008-08-12 18:11:09 +0000

    xslt build instruction on window

---
+++ trunk/fop_miniscribus.2.0.0/dev_doc/mysbuild.sh
@@ -0,0 +1,55 @@
+#! /bin/sh -e
+
+
+mkdir -p /C/Programming/libs/src
+mkdir -p /C/Programming/libs/include
+mkdir -p /C/Programming/libs/lib
+
+cd /C/Programming/libs/src
+
+
+wget http://ftp.gnu.org/gnu/libiconv/libiconv-1.11.tar.gz
+tar xzf libiconv-1.11.tar.gz
+rm libiconv-1.11.tar.gz
+cd libiconv-1.11
+./configure --enable-static --disable-shared --prefix=$PWD/../../
+make
+make install
+
+cd /C/Programming/libs/src
+
+
+wget http://www.exiv2.org/exiv2-0.12.tar.gz
+tar xzf exiv2-0.12.tar.gz
+rm exiv2-0.12.tar.gz
+cd exiv2-0.12
+./configure --enable-static --disable-shared --prefix=$PWD/../../ \
+CXXFLAGS="${CXXFLAGS} -DSUPPRESS_WARNINGS" --with-zlib=$PWD/../../
+make
+make install
+
+
+
+cd /C/Programming/libs/src
+
+wget ftp://xmlsoft.org/libxml2/libxml2-2.6.26.tar.gz
+tar xzf libxml2-2.6.26.tar.gz
+rm libxml2-2.6.26.tar.gz
+cd libxml2-2.6.26
+./configure --enable-static --disable-shared --with-iconv=$PWD/../../ --prefix=$PWD/../../
+make
+make install
+
+cd /C/Programming/libs/src
+
+wget ftp://xmlsoft.org/libxslt/libxslt-1.1.17.tar.gz
+tar xzf libxslt-1.1.17.tar.gz
+rm libxslt-1.1.17.tar.gz
+cd libxslt-1.1.17
+./configure --enable-static --disable-shared --with-libxml-prefix=$PWD/../../ --prefix=$PWD/../../
+make
+make install
+
+
+cd /C/Programming/libs/src
+
