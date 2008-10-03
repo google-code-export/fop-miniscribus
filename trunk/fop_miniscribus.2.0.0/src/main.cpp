08503e385681622de3c30ad58c66be2c9b616248
ppkciz <ppkciz@9af58faf-7e3e-0410-b956-55d145112073>
2008-10-03 21:06:54 +0000

    if static build fix image plug-in

--- trunk/fop_miniscribus.2.0.0/src/main.cpp
+++ trunk/fop_miniscribus.2.0.0/src/main.cpp
@@ -4,6 +4,31 @@
 #include "SessionManager.h"
 #include "MainWindow.h"
 
+
+#if defined _COMPOSE_STATIC_
+#include <QtPlugin>
+#if defined _USE_qjpeg
+Q_IMPORT_PLUGIN(qjpeg)
+#endif
+#if defined _USE_qmng
+Q_IMPORT_PLUGIN(qmng)
+#endif
+#if defined _USE_qgif
+Q_IMPORT_PLUGIN(qgif)
+#endif
+#if defined _USE_qtiff
+Q_IMPORT_PLUGIN(qtiff)
+#endif
+#endif
+
+
+
+
+
+
+
+
+
 int main( int argc, char ** argv )
 {
     QApplication app( argc, argv );
